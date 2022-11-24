077aee953fa37ad28ddfae6824f5a871422754f5
Pavel Ivanov <56752723+pavelivanov376@users.noreply.github.com>
2022-11-24 22:45:36 +0200

    Add files via upload

---
+++ webapp/src/main/java/com/pavel/webapp/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.pavel.webapp.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @GetMapping("/")
+    public @ResponseBody String getStr() {
+
+        return " Hello world";
+    }
+}
